d2ed3d4d6962db80bd6a524c30663ed3e964ffa7
Leslythomasmathew <Leslymathew1306@gmail.com>
2024-10-04 16:28:37 +0530

    Add files via upload

---
+++ lesly.py
@@ -0,0 +1,24 @@
+ 
+'''
+x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+
+x = "python"
+y = "is"
+z = "awesome"
+print(x + y+ z)
+   
+username = input("Enter username:")
+print(" your username is:" + username)
+'''
+'''
+a="hello"
+b="world"
+c= a + b
+print(c)
+'''
+a ="Hello, World!"
+print(a[6])
+
