f44b47de5ea2335b678f07cec67b4a45777a419c
Leslythomasmathew <Leslymathew1306@gmail.com>
2024-10-18 15:10:17 +0530

    Add files via upload

---
+++ firstpythonprogram.py
@@ -0,0 +1,16 @@
+'''
+Author:Lesly Mathew
+Date:18-10-2024
+Description:Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+Expected Output:
+
+Enter your name: John
+
+Enter your age: 25
+
+Hello, John! You are 25 years old.
+'''
+
+username=input("Enter your name :")
+age=input("Enter your age:")
+print("Hello,",username+"! You are",age,"years old")
