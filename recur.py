52f9eca6bab5d6a16e6bffdafcca48288a5ba84a
Leslythomasmathew <Leslymathew1306@gmail.com>
2024-11-30 16:28:23 +0530

    Add files via upload

--- recur.py
+++ recur.py
@@ -1,6 +1,12 @@
 def gcd(num1,num2):
     if num1%num2==0:
         return num2
+
     else:
         return gcd(num2,num1%num2)
-print(gcd(1220,516))
+
+n1 = int(input("Enter a number:"))
+n2 = int(input("Enter a number:"))
+print("The GCD of the given numbers is ", gcd(n1, n2))
+
+
